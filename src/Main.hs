e80e719e4881e7dcf070195d21ac28c2e247f354
justusadam <development@justusadam.com>
2015-06-15 16:41:37 +0200

    this seems prettier

--- src/Main.hs
+++ src/Main.hs
@@ -18,6 +18,11 @@ import           Network.Wai                (Application, Request (..),
                                              responseLBS)
 import           Network.Wai.Handler.Warp   (run)
 
+
+usePort :: Int
+usePort = 62947
+
+
 data Options = Options
   { gx                 :: Int
   , gy                 :: Int
@@ -45,13 +50,14 @@ genColors opts = map (niceGradient . (/maxval)) [0..maxval]
   where maxval = fromIntegral $ nColors opts - 1
 
 pickColor :: Options -> Float -> (Int, Int, Int)
-pickColor opts = if fNColors > 0
-                 then (genColors opts !!)
-                    . round
-                    . (\x -> if x == 1.0
-                             then fNColors - 1
-                             else x * fNColors - 0.5) -- round (0-0.5) = 0
-                 else niceGradient
+pickColor opts = 
+  if fNColors > 0
+    then (genColors opts !!)
+      . round
+      . (\x -> if x == 1.0
+                 then fNColors - 1
+                 else x * fNColors - 0.5) -- round (0-0.5) = 0
+    else niceGradient
   where fNColors = fromIntegral $ nColors opts
 
 -- Since the base color is (87, 59, 117), f * (0.74359, 0.50427, 1) generates all colors.
@@ -61,20 +67,21 @@ niceGradient f = (round $ 255 * f * 0.74359, round $ 255 * f * 0.50427, round $ 
 -- | clamps to 1.0 maximum for radius < 1
 intensityScalar :: Options -> Float -> (Int, Int) -> Float
 intensityScalar opts rndval (x, y) = inv $ min (max (result + randompart) 0) 1
-    where
-        randompart = randomness opts * (rndval * 2 - 1)
-        result = (quantize $ pytha x y) / (radius opts * pytha 0 0)
-        pytha p q = rsq $ (diff p (gx opts)) + (diff q (gy opts))
-        rsq = sqrt . fromIntegral
-        diff a ga = quad $ abs (a - (ga `div` 2))
-        quad x = x*x
-        quantize = if quantizePythagoras opts then fromIntegral . round else id
-        inv x = if invert opts then 1-x else x
+  where
+    randompart = randomness opts * (rndval * 2 - 1)
+    result     = (quantize $ pytha x y) / (radius opts * pytha 0 0)
+    pytha p q  = rsq $ (diff p (gx opts)) + (diff q (gy opts))
+    rsq        = sqrt . fromIntegral
+    diff a ga  = quad $ abs (a - (ga `div` 2))
+    quad x     = x*x
+    quantize   = if quantizePythagoras opts then fromIntegral . round else id
+    inv x      = if invert opts then 1-x else x
 
 svgPixel :: Options -> (Int, Int) -> (Int, Int, Int) -> String
-svgPixel opts (x,y) (r,g,b) =
-  let s = scale opts
-  in printf "<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" stroke=\"none\" stroke-width=\"0\" fill=\"#%02x%02x%02x\"/>" (s*x - 1) (s*y - 1) (s + 2) (s + 2) r g b
+svgPixel (Options { scale = scale }) (x,y) (r,g,b) =
+  printf 
+    "<rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" stroke=\"none\" stroke-width=\"0\" fill=\"#%02x%02x%02x\"/>" 
+    (scale*x - 1) (scale*y - 1) (scale + 2) (scale + 2) r g b
 
 evalPixel :: Options -> (Int, Int) -> State StdGen String
 evalPixel opts p = do
@@ -84,11 +91,25 @@ evalPixel opts p = do
     return $ svgPixel opts p $ pickColor opts $ intensityScalar opts rndval p
 
 svgPic :: Options -> String
-svgPic opts = let rawpixels = [(x,y) | x <- [0..(gx opts - 1)], y <- [0..(gy opts - 1)]]
-                  pixels = evalState (mapM (evalPixel opts) rawpixels) (mkStdGen 42)
-                  width = show $ scale opts * gx opts
-                  height = show $ scale opts * gy opts
-              in printf "<?xml version=\"1.0\" standalone=\"no\"?><svg version=\"1.1\" baseProfile=\"full\" width=\"%s\" height=\"%s\" xmlns=\"http://www.w3.org/2000/svg\">%s</svg><!-- Generated with %s -->" width height (concat pixels) (show opts)
+svgPic opts@(Options { scale = scale
+                     , gx    = gx
+                     , gy    = gy
+                     }) = 
+  let 
+    rawpixels = [(x,y) | x <- [0..(gx - 1)], y <- [0..(gy - 1)]]
+    pixels    = evalState (mapM (evalPixel opts) rawpixels) (mkStdGen 42)
+    width     = show $ scale * gx
+    height    = show $ scale * gy
+  in 
+    printf 
+      "<?xml version=\"1.0\" standalone=\"no\"?>\
+      \<svg version=\"1.1\" baseProfile=\"full\" width=\"%s\" height=\"%s\" xmlns=\"http://www.w3.org/2000/svg\">\
+      \%s\
+      \</svg><!-- Generated with %s -->" 
+      width 
+      height 
+      (concat pixels) 
+      (show opts)
 
 
 app :: Application
@@ -109,4 +130,4 @@ app req respond = respond $ responseLBS status200 [(CI.mk "Content-Type", "image
           , ( "invert"           , fmap (\i -> opts { invert             = i }) . readMaybe)
           ]
 
-main = run 62947 app
+main = run usePort app
