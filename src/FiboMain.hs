25f63bd86f4c8f762e18dbd2fc474e7fec08c301
Sebastian J. Mielke <sjm@sjmielke.com>
2016-04-08 17:45:58 +0200

    "Blending" between triangulations. Also, suddenly lots of hardcoding magic numbers.

--- src/FiboMain.hs
+++ src/FiboMain.hs
@@ -23,15 +23,17 @@ type Color = (Int, Int, Int)
 data Options = Options
   { numPoints :: Int
   , distRoot :: Float
-  , lineSkips :: [Int]
+  , radius :: Int
+  , lineWidth :: Int
   , size :: Int
   } deriving Show
 
 defaultOptions = Options
-  { numPoints = 1000
-  , distRoot = 0.5
-  , lineSkips = [8, 9, 10]
-  , size = 500
+  { numPoints = 200
+  , distRoot = 0.8
+  , radius = 7
+  , lineWidth = 2
+  , size = 700
   }
 
 fiboNumbers :: [Int]
@@ -59,14 +61,21 @@ renderPoint opts (x, y) = do
         ( "<circle cx=\"%d\" cy=\"%d\" r=\"%d\" " ++
           "stroke=\"none\" stroke-width=\"0\" " ++
           "fill=\"#%02x%02x%02x\"/>" )
-        x y (10 :: Int)
-        (255 :: Int) (round $ 180.0  - 25.0 + 50.0 * rndval :: Int) (0 :: Int)
+        x y (radius opts)
+        (255 :: Int)
+        (round $ 180.0  - 25.0 + 50.0 * rndval :: Int)
+        (0 :: Int)
 
-renderLine :: ((Int, Int), (Int, Int)) -> String
-renderLine ((x1, y1), (x2, y2))
+renderLine :: Options -> Float -> ((Int, Int), (Int, Int)) -> String
+renderLine opts distancish ((x1, y1), (x2, y2))
   = printf ( "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" " ++
-             "stroke=\"black\" stroke-width=\"2\"/>" )
+             "stroke=\"#%02x%02x%02x\" stroke-width=\"%d\" " ++
+             "stroke-linecap=\"round\"/>" )
            x1 y1 x2 y2
+           (round $ 255 * (distancish ** 0.05) :: Int)
+           (min 255 $ round $ 170.0 + 8000.0 * (1.0 - distancish ** 0.002) :: Int)
+           (0 :: Int)
+           (lineWidth opts)
 
 normalizePoints :: Options -> [(Float, Float)] -> [(Int, Int)]
 normalizePoints opts xs
@@ -78,17 +87,30 @@ normalizePoints opts xs
     miny = minimum $ map snd xs
     maxy = maximum $ map snd xs
 
-linesFor :: [(Int, Int)] -> Int -> String
-linesFor points skip
-  = concatMap renderLine
-  $ drop (fiboNumbers !! skip)
-  $ zip points (replicate (fiboNumbers !! skip) (350,350) ++ points)
+linesFor
+  :: Options
+  -> Int -- ^ point at which interval to triangulate on starts
+  -> Int -- ^ length of that interval
+  -> [(Int, Int)] -- ^ all (!) points
+  -> Int -- ^ actual lineskip
+  -> String
+linesFor opts from length points skip
+  = concatMap (uncurry $ renderLine opts)
+  $ zip (map ((/(fromIntegral $ numPoints opts)) . fromIntegral) [1..])
+  $ take length $ drop from
+  $ allPairs
+  where
+    -- Actually contains all pairs: p_0/p_skip, p_1/p_{skip+1} ...
+    allPairs = drop (fiboNumbers !! skip)
+             $ zip points (replicate (fiboNumbers !! skip) (0, 0) ++ points)
 
 svgPic :: Options -> String
 svgPic opts
   = let points = normalizePoints opts $ take (numPoints opts) $ getFiboPoints opts
-        pixels = concat $ evalState (mapM (renderPoint opts) points) (mkStdGen 42)
-        lines = concatMap (linesFor points) (lineSkips opts)
+        circles = concat $ evalState (mapM (renderPoint opts) points) (mkStdGen 42)
+        lines =  concatMap (linesFor opts 5 15 points) [5..7]
+              ++ concatMap (linesFor opts 12 27 points) [6..8]
+              ++ concatMap (linesFor opts 26 9001 points) [7..9]
         width = show $ size opts + 42
         height = show $ size opts + 42
     in printf ( "<?xml version=\"1.0\" standalone=\"no\"?>" ++
@@ -97,17 +119,18 @@ svgPic opts
                 "xmlns=\"http://www.w3.org/2000/svg\">%s</svg>" ++
                 "<!-- Generated with %s -->" )
               width height
-              (pixels ++ lines)
+              (lines ++ circles)
               (show opts)
 
 app :: Application
 app req respond = respond $ responseLBS status200 [(CI.mk "Content-Type", "image/svg+xml")] (BSL.pack pic)
   where
     pic = svgPic $ foldl modify defaultOptions (queryString req)
-    modify opts ("numPoints",  Just s) = case readMaybe $ BS.unpack s of Just i -> opts {numPoints  = i}; Nothing -> opts
-    modify opts ("distRoot",   Just s) = case readMaybe $ BS.unpack s of Just i -> opts {distRoot   = i}; Nothing -> opts
-    modify opts ("lineSkips",  Just s) = case readMaybe $ BS.unpack s of Just i -> opts {lineSkips  = i}; Nothing -> opts
-    modify opts ("size",       Just s) = case readMaybe $ BS.unpack s of Just i -> opts {size       = i}; Nothing -> opts
+    modify opts ("numPoints", Just s) = case readMaybe $ BS.unpack s of Just i -> opts {numPoints = i}; Nothing -> opts
+    modify opts ("distRoot",  Just s) = case readMaybe $ BS.unpack s of Just i -> opts {distRoot  = i}; Nothing -> opts
+    modify opts ("radius",    Just s) = case readMaybe $ BS.unpack s of Just i -> opts {radius    = i}; Nothing -> opts
+    modify opts ("lineWidth", Just s) = case readMaybe $ BS.unpack s of Just i -> opts {lineWidth = i}; Nothing -> opts
+    modify opts ("size",      Just s) = case readMaybe $ BS.unpack s of Just i -> opts {size      = i}; Nothing -> opts
     modify opts _ = opts
 
 main = run 62947 app
